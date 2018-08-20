b7a8490e82c047274bd7de61effe43e0fb66b55f
lina thedog <koza-sparrow@hotmail.com>
2018-08-20 12:23:11 +0300

    cleaned up some templated and components

--- app/models/library.js
+++ app/models/library.js
@@ -1,7 +1,11 @@
 import DS from 'ember-data';
+import { notEmpty } from '@ember/object/computed';
 
 export default DS.Model.extend({
+
   name: DS.attr('string'),
   address: DS.attr('string'),
-  phone: DS.attr('string')
+  phone: DS.attr('string'),
+
+  isValid: notEmpty('name')
 });
--- app/routes/libraries/new.js
+++ app/routes/libraries/new.js
@@ -1,9 +1,43 @@
+// import Route from '@ember/routing/route';
+//
+// export default Route.extend({
+//
+//   model() {
+//     return this.store.createRecord('library');
+//   },
+//
+//   actions: {
+//
+//     saveLibrary(newLibrary) {
+//       newLibrary.save().then(() => this.transitionTo('libraries'));
+//     },
+//
+//     willTransition() {
+//       // rollbackAttributes() removes the record from the store
+//       // if the model 'isNew'
+//       this.controller.get('model').rollbackAttributes();
+//     }
+//   }
+// });
+
+
 import Route from '@ember/routing/route';
 
 export default Route.extend({
 
   model() {
     return this.store.createRecord('library');
+  },
+
+  setupController(controller, model) {
+    this._super(controller, model);
+
+    controller.set('title', 'Create a new library');
+    controller.set('buttonLabel', 'Create');
+  },
+
+  renderTemplate() {
+    this.render('libraries/form');
   },
 
   actions: {
@@ -13,9 +47,8 @@ export default Route.extend({
     },
 
     willTransition() {
-      // rollbackAttributes() removes the record from the store
-      // if the model 'isNew'
       this.controller.get('model').rollbackAttributes();
+      }
     }
-  }
+
 });
